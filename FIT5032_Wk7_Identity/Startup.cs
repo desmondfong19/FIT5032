9edeb9ae1b1f6bc34751fc7c4dd5c12b1d6cb906
desmondfong19 <69134155+desmondfong19@users.noreply.github.com>
2021-09-17 21:27:40 +1000

    Add project files.

---
+++ FIT5032_Wk7_Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Wk7_Identity.Startup))]
+namespace FIT5032_Wk7_Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
